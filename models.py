c9579ab0212c55f64f98d830a2a9fff4314cf057
wmh117 <minghanwu99@gmail.com>
2026-03-18 11:50:13 +0800

    部门显示已完成

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Department(models.Model):
     """Department model部门表"""
     title = models.CharField(max_length=32,verbose_name='部门标题')
@@ -11,3 +12,9 @@ class UserInfo(models.Model):
     age = models.IntegerField(verbose_name='员工年龄')
     account = models.DecimalField(max_digits=10,decimal_places=2,default=0,verbose_name='账户余额')
     create_time = models.DateTimeField(verbose_name='入职时间')
+    depart = models.ForeignKey(to = "Department",to_field='id',on_delete=models.CASCADE)
+    gender_choices = (
+    (1,'男'),
+    (2,'女'),
+    )
+    gender = models.SmallIntegerField(verbose_name='性别',choices=gender_choices)
--- views.py
+++ views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from manageapp import models
+from manageapp.models import Department
+
 
 # Create your views here.
+def depart_list(request):
+    """部门列表"""
+    queryset = Department.objects.all()
+    return render(request,'depart_list.html',{'queryset':queryset})#可以传递参数了
+
+def depart_add(request):
+    """添加部门"""
+    return render(request,'depart_add.html')
