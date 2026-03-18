91629beaf9ff975010e0f93f8b9e0f9037eea88d
wmh117 <minghanwu99@gmail.com>
2026-03-18 17:22:20 +0800

    部门增删改完成

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from manageapp import models
 from manageapp.models import Department
 
@@ -11,4 +12,18 @@ def depart_list(request):
 
 def depart_add(request):
     """添加部门"""
-    return render(request,'depart_add.html')
+    if request.method == 'GET':
+        return render(request,'depart_add.html')
+    title = request.POST.get('title')
+    models.Department.objects.create(title=title)
+    return redirect('/depart/list/')
+def depart_delete(request):
+    nid = request.GET.get('nid')
+    models.Department.objects.filter(id = nid).delete()
+    return redirect('/depart/list/')
+def depart_edit(request,nid):
+    if request.method == 'GET':
+        row_object = models.Department.objects.filter(id = nid).first()
+        return render(request,'depart_edit.html',{'row_object':row_object})
+    models.Department.objects.filter(id = nid).update(title=request.POST.get('title'))
+    return redirect('/depart/list/')
