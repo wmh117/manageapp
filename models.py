6d4b6e0c53a6581300a97a934456be4a7f7978f9
wmh117 <minghanwu99@gmail.com>
2026-03-31 11:46:31 +0800

    管理员登陆 cookie验证

--- models.py
+++ models.py
@@ -40,3 +40,12 @@ class PrettyNum(models.Model):
     )
     status = models.SmallIntegerField(choices=status_choices,default=2,verbose_name="状态")
 
+# models.py
+
+class Admin(models.Model):
+    """ 管理员表 """
+    username = models.CharField(verbose_name="用户名", max_length=32)
+    password = models.CharField(verbose_name="密码", max_length=64)
+
+    def __str__(self):
+        return self.username
